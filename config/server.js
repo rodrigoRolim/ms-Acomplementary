789043623d27c226763902ba59d4fc13f1407f7a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-10-20 00:18:55 -0300

    final config server

--- config/server.js
+++ config/server.js
@@ -1,11 +1,26 @@
+'use strict'
+
 const express = require('express')
 const app = express()
 const consign = require('consign')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const cors = require('cors')
+const path = require('path')
+const mongoose = require('mongoose')
+const dbConnection = require('./dbConnection')
 
-app.set('views', './app/views')
-app.use(express.static('./app/public'))
+mongoose.connect(dbConnection.URI, dbConnection.options)
+
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+    console.log('connect to database')
+})
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extend: true }))
 app.use(expressValidator())
 
@@ -15,5 +30,6 @@ consign()
     .then('app/controllers')
     .then('config/dbConnection.js')
     .into(app)
+
 
 module.exports = app
