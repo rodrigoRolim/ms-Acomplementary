d4b51ca0c64c37379b9cd20f461e759c2064889a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-07 18:23:00 -0200

    refactor for added promise

--- config/server.js
+++ config/server.js
@@ -9,27 +9,39 @@ const cors = require('cors')
 const path = require('path')
 const mongoose = require('mongoose')
 const dbConnection = require('./dbConnection')
+const env = require('./enviroments');
 
-mongoose.connect(dbConnection.URI, dbConnection.options)
+function intializeDb() {
+  mongoose.Promise = global.Promise;
+  return mongoose.connect(dbConnection.URI, dbConnection.options)
+}
 
-var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
-    console.log('connect to database')
-})
-
-app.use(cors())
-app.use(express.static(path.join(__dirname, '/public')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extend: true }))
-app.use(expressValidator())
-
-consign()
-    .include('app/routes')
-    .then('app/models')
-    .then('app/controllers')
-    .then('config/dbConnection.js')
-    .into(app)
-
-
-module.exports = app
+function buildApp() {
+  return new Promise((resolve, reject) => {
+    try {
+      app.use(cors())
+      app.use(express.static(path.join(__dirname, '/public')))
+      app.use(bodyParser.json())
+      app.use(bodyParser.urlencoded({ extended: true }))
+      app.use(expressValidator())
+      consign()
+        .include('app/routes')
+        .then('app/models')
+        .then('app/controllers')
+        .then('config/dbConnection.js')
+        .into(app)
+      app.listen(env.port, () => {
+          resolve(app)
+      })
+    } catch(error) {
+      reject(error)
+    }
+    
+  })
+  
+}
+module.exports = function() {
+  return intializeDb().then(() => {
+      buildApp().then(() => this)
+  })
+}
