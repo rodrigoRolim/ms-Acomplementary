8f7135383f2032d53a7d243ee7771255a5032ca2
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-07 18:51:19 -0200

    update from callback to promise

--- app/controllers/StudentController.js
+++ app/controllers/StudentController.js
@@ -3,8 +3,8 @@ var Student = require('../models/Students.model')
 var StudentController = {}
 
 StudentController.list = function (req, res, next) {
-  Student.model.find().then(users => {
-    res.json(users)
+  Student.model.find().then(students => {
+    res.json(students)
     return next()
   })
 }
@@ -12,38 +12,36 @@ StudentController.save = function (req, res, next) {
   let student = new app.models.Student(req.body)
   Student.model.save({student}).then(msg => {
     res.json({sucess: msg})
-    return next(false)
+    return next()
   }).catch(err => {
     res.json({error: err})
     return next()
   })
 }
-StudentController.edit = function (req, res, app) {
-  Student.model.findOne({_id : req.params.id}).exec(function (err, student) {
-    if (err) {
-      console.log("Error:", err)
-    } else {
-        console.log("do response")
-    }
+StudentController.edit = function (req, res, next) {
+  Student.model.findOne({_id : req.params.id}).then((student) => {
+    res.json(student)
+    return next()
+  }).catch((err) => {
+    res.json({error: err})
+    return next()
   }) 
 }
 StudentController.update = function (req, res, app) {
-  Student.findByIdAndUpdate(req.params.id, { $set: {}}, { new: true}, function (err, student) {
-    if (err) {
-      console.log(err)
-      // set view here
-    } else {
-      // response
-    }
+  Student.findByIdAndUpdate(req.params.id, { $set: {}}, { new: true}).then((student) => {
+    res.json(student)
+    return next()
+  }).catch((err) => {
+    res.json({error: err})
+    return next()
   })
 }
 StudentController.delete = function (req, res) {
-  Student.remove({_id: req.params.id}, function () {
-    if (err) {
-      // todo
-    } else {
-      // todo
-    }
+  Student.remove({_id: req.params.id}).then((student) => {
+    res.json(student)
+    return next()
+  }).catch((err) => {
+    res.json({error: err})
   })
 }
 module.exports = StudentController;
